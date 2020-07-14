e3e9aae1f5763285f82af44e2740717f89a99598
haomiao.yin <haomiao.yin@esimtek.com>
2020-07-14 10:51:07 +0800

    modify extera in and out data in SHInvFragment and SHVertifyFragment

--- app/src/main/java/com/zebra/rfidreader/nonghang/nongshanghang/SHInvFragment.java
+++ app/src/main/java/com/zebra/rfidreader/nonghang/nongshanghang/SHInvFragment.java
@@ -48,7 +48,6 @@ import java.util.Set;
 import static android.app.Activity.RESULT_OK;
 
 public class SHInvFragment extends Fragment implements ResponseHandlerInterfaces.ResponseTagHandler, ResponseHandlerInterfaces.TriggerEventHandler, FileBeanAdapter.OnItemClickListener, EpcBeanAdapter.OnItemClickListener {
-    private Button btRead;
     private Button btWrite;
     private Button chooseBox;
     ArrayList<FileBean> currentFileList = new ArrayList<>();
@@ -106,21 +105,11 @@ public class SHInvFragment extends Fragment implements ResponseHandlerInterfaces
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        btRead = (Button) getActivity().findViewById(R.id.bt_read);
         btWrite = (Button) getActivity().findViewById(R.id.bt_write);
         chooseBox = (Button) getActivity().findViewById(R.id.bt_choose_box);
         inventoryButton = (FloatingActionButton) getActivity().findViewById(R.id.inventoryButton);
         selectFileNum = (TextView) getActivity().findViewById(R.id.file_num);
         invNum = (TextView) getActivity().findViewById(R.id.inv_num);
-        btRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("*/*");//设置任意类型
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent, 1);
-            }
-        });
 
         btWrite.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -350,49 +339,6 @@ public class SHInvFragment extends Fragment implements ResponseHandlerInterfaces
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                importExcle(data);
-            }
-        }
-    }
-
-    /**
-     * 导入选中文件
-     */
-    public void importExcle(Intent data) {
-        Uri uri = data.getData();
-        if (uri != null) {
-            String path = getPath(getContext(), uri);
-            if (path != null) {
-                File file = new File(path);
-                if (file.exists()) {
-                    //获取的路径
-                    String upLoadFilePath = file.toString();
-                    //文件名
-                    String upLoadFileName = file.getName();
-                    String[] strArray = upLoadFileName.split("\\.");
-                    int suffixIndex = strArray.length - 1;
-                    File dir = new File(upLoadFilePath);
-                    //调用查询方法
-                    //ExcelUtil.QueryUser(this, dir);
-                    List<FileBean> fileBeans = ExcelUtils.read2DB(dir, getActivity());
-                    excelfileBeans.clear();
-                    excelfileBeans.addAll(fileBeans);
-                    divideByBoxcode(excelfileBeans);
-                    divideByBag(excelfileBeans);
-                    fileBeanAdapter.notifyDataSetChanged();
-                    DemoDatabase.getInstance().getFileBeanDao().deleteAllData();
-                    DemoDatabase.getInstance().getFileBeanDao().insertItems(excelfileBeans);
-                    Toast.makeText(getActivity(), excelfileBeans.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
     private void divideByBoxcode(List<FileBean> fileBeans) {
         multiAdapter.removeData(multiDatas);
         multiDatas.clear();
@@ -405,97 +351,6 @@ public class SHInvFragment extends Fragment implements ResponseHandlerInterfaces
             }
         }
         multiAdapter.addData(multiDatas);
-    }
-
-    public String getPath(final Context context, final Uri uri) {
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-            } else if (isDownloadsDocument(uri)) {
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{split[1]};
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-//            Log.i(TAG,"content***"+uri.toString());
-            return getDataColumn(context, uri, null, null);
-        }
-        // Files
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-//            Log.i(TAG,"file***"+uri.toString());
-            return uri.getPath();
-        }
-        return null;
-    }
-
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context       The context.
-     * @param uri           The Uri to query.
-     * @param selection     (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    public String getDataColumn(Context context, Uri uri, String selection,
-                                String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-    public boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    public boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    public boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
     public void divideByBag(List<FileBean> fileBeans) {
--- app/src/main/java/com/zebra/rfidreader/nonghang/nongshanghang/SHVertifyFragment.java
+++ app/src/main/java/com/zebra/rfidreader/nonghang/nongshanghang/SHVertifyFragment.java
@@ -49,8 +49,6 @@ import static android.app.Activity.RESULT_OK;
 import static android.content.Context.INPUT_METHOD_SERVICE;
 
 public class SHVertifyFragment extends Fragment implements ResponseHandlerInterfaces.ResponseTagHandler, ResponseHandlerInterfaces.TriggerEventHandler, FileBeanVertifyAdapter.OnItemClickListener, EpcBeanAdapter.OnItemClickListener {
-    private Button btRead;
-    private Button btWrite;
     private Button chooseBox;
     //当前按封袋编号处理过的集合
     ArrayList<FileBean> currentFileList = new ArrayList<>();
@@ -115,8 +113,6 @@ public class SHVertifyFragment extends Fragment implements ResponseHandlerInterf
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
-        btRead = (Button) getActivity().findViewById(R.id.bt_read);
-        btWrite = (Button) getActivity().findViewById(R.id.bt_write);
         chooseBox = (Button) getActivity().findViewById(R.id.bt_choose_box);
         inventoryButton = (FloatingActionButton) getActivity().findViewById(R.id.inventoryButton);
         selectFileNum = (TextView) getActivity().findViewById(R.id.file_num);
@@ -124,37 +120,6 @@ public class SHVertifyFragment extends Fragment implements ResponseHandlerInterf
         mSearchData =  getActivity().findViewById(R.id.et_searvh);
         searchButton =  getActivity().findViewById(R.id.bt_search);
         result =  getActivity().findViewById(R.id.tv_result);
-        btRead.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
-                intent.setType("*/*");//设置任意类型
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                startActivityForResult(intent, 1);
-            }
-        });
-
-        btWrite.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                ArrayList<FileBean> writeBeans = new ArrayList<>();
-                for (FileBean fileBean : currentFileList) {
-                    for (EpcBean epc : fileBean.getEpcs()) {
-                        FileBean toFileBean = new FileBean();
-                        copyFileBean(fileBean, toFileBean);
-                        toFileBean.setEpcCode(epc.getEpc());
-                        toFileBean.setInvStatus(epc.isInved());
-                        writeBeans.add(toFileBean);
-                    }
-                }
-                try {
-                    ExcelUtils.writeExcel(getContext(), writeBeans, "excelResult.xls");
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        });
-
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -385,49 +350,6 @@ public class SHVertifyFragment extends Fragment implements ResponseHandlerInterf
         }
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1) {
-            if (resultCode == RESULT_OK) {
-                importExcle(data);
-            }
-        }
-    }
-
-    /**
-     * 导入选中文件
-     */
-    public void importExcle(Intent data) {
-        Uri uri = data.getData();
-        if (uri != null) {
-            String path = getPath(getContext(), uri);
-            if (path != null) {
-                File file = new File(path);
-                if (file.exists()) {
-                    //获取的路径
-                    String upLoadFilePath = file.toString();
-                    //文件名
-                    String upLoadFileName = file.getName();
-                    String[] strArray = upLoadFileName.split("\\.");
-                    int suffixIndex = strArray.length - 1;
-                    File dir = new File(upLoadFilePath);
-                    //调用查询方法
-                    //ExcelUtil.QueryUser(this, dir);
-                    List<FileBean> fileBeans = ExcelUtils.read2DB(dir, getActivity());
-                    excelfileBeans.clear();
-                    excelfileBeans.addAll(fileBeans);
-                    divideByBoxcode(excelfileBeans);
-                    divideByBag(excelfileBeans);
-                    fileBeanAdapter.notifyDataSetChanged();
-                    DemoDatabase.getInstance().getFileBeanDao().deleteAllData();
-                    DemoDatabase.getInstance().getFileBeanDao().insertItems(excelfileBeans);
-                    Toast.makeText(getActivity(), excelfileBeans.toString(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }
-    }
-
     private void divideByBoxcode(List<FileBean> fileBeans) {
         multiDatas.clear();
         ArrayList<String> boxCodes = new ArrayList<>();
@@ -438,97 +360,6 @@ public class SHVertifyFragment extends Fragment implements ResponseHandlerInterf
             }
         }
         multiAdapter.notifyDataSetChanged();
-    }
-
-    public String getPath(final Context context, final Uri uri) {
-        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
-        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
-            if (isExternalStorageDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-
-                if ("primary".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/" + split[1];
-                }
-            } else if (isDownloadsDocument(uri)) {
-                final String id = DocumentsContract.getDocumentId(uri);
-                final Uri contentUri = ContentUris.withAppendedId(
-                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
-                return getDataColumn(context, contentUri, null, null);
-            }
-            // MediaProvider
-            else if (isMediaDocument(uri)) {
-                final String docId = DocumentsContract.getDocumentId(uri);
-                final String[] split = docId.split(":");
-                final String type = split[0];
-                Uri contentUri = null;
-                if ("image".equals(type)) {
-                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                } else if ("video".equals(type)) {
-                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                } else if ("audio".equals(type)) {
-                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                }
-                final String selection = "_id=?";
-                final String[] selectionArgs = new String[]{split[1]};
-                return getDataColumn(context, contentUri, selection, selectionArgs);
-            }
-        }
-        // MediaStore (and general)
-        else if ("content".equalsIgnoreCase(uri.getScheme())) {
-//            Log.i(TAG,"content***"+uri.toString());
-            return getDataColumn(context, uri, null, null);
-        }
-        // Files
-        else if ("file".equalsIgnoreCase(uri.getScheme())) {
-//            Log.i(TAG,"file***"+uri.toString());
-            return uri.getPath();
-        }
-        return null;
-    }
-
-    /**
-     * Get the value of the data column for this Uri. This is useful for
-     * MediaStore Uris, and other file-based ContentProviders.
-     *
-     * @param context       The context.
-     * @param uri           The Uri to query.
-     * @param selection     (Optional) Filter used in the query.
-     * @param selectionArgs (Optional) Selection arguments used in the query.
-     * @return The value of the _data column, which is typically a file path.
-     */
-    public String getDataColumn(Context context, Uri uri, String selection,
-                                String[] selectionArgs) {
-
-        Cursor cursor = null;
-        final String column = "_data";
-        final String[] projection = {column};
-
-        try {
-            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
-                    null);
-            if (cursor != null && cursor.moveToFirst()) {
-                final int column_index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(column_index);
-            }
-        } finally {
-            if (cursor != null)
-                cursor.close();
-        }
-        return null;
-    }
-
-    public boolean isExternalStorageDocument(Uri uri) {
-        return "com.android.externalstorage.documents".equals(uri.getAuthority());
-    }
-
-    public boolean isDownloadsDocument(Uri uri) {
-        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
-    }
-
-    public boolean isMediaDocument(Uri uri) {
-        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 
     public void divideByBag(List<FileBean> fileBeans) {
